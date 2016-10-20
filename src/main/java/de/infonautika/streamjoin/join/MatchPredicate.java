c77c3faf6ef49a8e65acf5341ad4186b57b5a04f
Matthias Simon <simon@infonautika.de>
2016-10-20 16:00:30 +0200

    removed compiler warning

--- src/main/java/de/infonautika/streamjoin/join/MatchPredicate.java
+++ src/main/java/de/infonautika/streamjoin/join/MatchPredicate.java
@@ -2,11 +2,12 @@ package de.infonautika.streamjoin.join;
 
 import java.util.function.BiPredicate;
 
+@SuppressWarnings("unchecked")
 public class MatchPredicate {
     static final BiPredicate<Object, Object> EQUALS = Object::equals;
 
     public static <KL, KR> BiPredicate<KL, KR> equals() {
-        //noinspection unchecked
+
         return (BiPredicate<KL, KR>)EQUALS;
     }
 }
